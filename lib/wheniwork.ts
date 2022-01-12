1c430c4c001c7c947f72d60c93b2a98deb91f57a
kara burnett <kara@foundrymakes.com>
2022-01-12 11:07:53 -0600

    err can be undefined, check for that first

--- lib/wheniwork.ts
+++ lib/wheniwork.ts
@@ -214,7 +214,7 @@ export class WIWError extends Error {
   constructor(err) {
     super(err);
     this.status = err.statusCode;
-    var data = err.error || err || { code: "unknown", error: err };
+    var data = err || err.error || { code: "unknown", error: err };
     this.code = data.code;
     this.message = `${this.code} - ${err.error.error}`;
   }
